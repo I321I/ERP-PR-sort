05fc077a74ddfa40fc7aa078474942d23dc06bac
i321i <i321ixd@gmail.com>
2026-02-18 09:02:31 +0000

    fix files name

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { Container } from 'react-bootstrap';
 import { Navigation } from './components/Navigation.tsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { InputField } from './components/nputField.tsx';
-import "./components/body.scss"
+import { InputField } from './components/InputField.tsx';
+import "./components/Body.scss"
 function App() {
   return (
     <>
